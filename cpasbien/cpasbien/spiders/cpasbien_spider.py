c36844a448e689afa0d31206097f12d4e18f1851
amandron <anthony.mandron@gmail.com>
2014-05-27 16:42:14 +0200

    quelques changements ...

--- cpasbien/cpasbien/spiders/cpasbien_spider.py
+++ cpasbien/cpasbien/spiders/cpasbien_spider.py
@@ -1,35 +1,33 @@
-from scrapy.spider import Spider
+from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from scrapy.http import Request
 
 from cpasbien.items import TorrentItem
 
-class CpasbienSpider(Spider):
+class CpasbienSpider(BaseSpider):
 
-	name = "cpasbien"
+ name = "cpasbien"
 
-	allowed_domains = [
-	"cpasbien.pe"
-	]
-	start_urls = [
-	'http://cpasbien.pe/recherche'
-	]
+ allowed_domains = [
+  "cpasbien.pe"
+ ]
 
-	def parse(self, response):
-		hxs = HtmlXPathSelector(response)
-		items = []
-		for entry in entries:
-			item = TorrentItem()
-			item['title'] = entry.select('td[1]/div[2]/a[2]/text()').extract()
-			for s in self.keywords:
-				if s.lower() in item['title'][0].lower():
-					item['url'] = entry.select('td[1]/div[2]/a[2]/@href').extract()
-					item['torrent'] = entry.select('td[1]/div[1]/a[starts-with(@title,"Download torrent file")]/@href').extract()
-					item['size'] = entry.select('td[2]/text()[1]').extract()
-					item['sizeType'] = entry.select('td[2]/span/text()').extract()
-					item['age'] = entry.select('td[4]/text()').extract()
-					item['seed'] = entry.select('td[5]/text()').extract()
-					item['leech'] = entry.select('td[6]/text()').extract()	
-					items.append(item)
-					break	
-		return items
+ def __init__(self, *args, **kwargs): 
+  super(CpasbienSpider, self).__init__(**kwargs)
+  self.keywords = kwargs['keywords'].split(',')
+  self.start_urls = [
+   'http://cpasbien.pe/recherche']
+
+ def parse(self, response):
+  items = []
+  for entry in entries:
+    item = TorrentItem()
+    item['name'] = entry.select("/div[@id='center-top']/href[1]/text()[2]").extract()
+    item['description'] = entry.select("//div[@id='description']").extract()
+    item['size'] = entry.select("//div[@id='informations-torrent']/strong[2]/text()[2]").extract()
+    item['url'] = entry.select("/div[@id=download-torrent']/href[1]/text()[2]").extract()
+    for s in self.keywords:
+     if s.lower() in item['title'][0].lower():
+      items.append(item)
+      break
+  return items
